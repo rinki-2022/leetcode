36b793e72487af36cab9fecd3d88f56115708808
rinki-2022 <rinkibansal2022@gmail.com>
2024-09-14 22:53:31 -0400

    meeting room

--- src/main/java/MeetingRooms.java
+++ src/main/java/MeetingRooms.java
@@ -1,5 +1,5 @@
 public class MeetingRooms {
     public static void main(String[] args) {
-        System.out.println("---Meeting Room problem---");
+        System.out.println("---Meeting Room -----");
     }
 }
