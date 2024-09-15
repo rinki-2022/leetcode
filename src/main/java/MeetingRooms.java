11ab880eb641e128f6545ad17e3f338d11d5e835
rinki-2022 <rinkibansal2022@gmail.com>
2024-09-14 23:14:25 -0400

    meeting room problem solved

--- src/main/java/MeetingRooms.java
+++ src/main/java/MeetingRooms.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
+
 public class MeetingRooms {
+    public static boolean canAttendMeetings(int[][] intervals) {
+
+        if (intervals.length == 0){
+            return true;
+        }
+
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
+        System.out.println(Arrays.deepToString(intervals));
+
+        for (int i=1; i< intervals.length;i++){
+            if(intervals[i][0] < intervals[i-1][1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        System.out.println("---Meeting Room -----");
+      //  int[][] intervals = { {0, 30}, {5, 10}, {15, 20} , {18,40}, {12,14}};
+        int[][] intervals = { {0, 3}, {5, 10}, {15, 20} };
+        System.out.println(canAttendMeetings(intervals));
     }
 }
