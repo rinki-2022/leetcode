145ae2226d39d3e344862ac128baca2e8e95a67b
rinki-2022 <rinkibansal2022@gmail.com>
2024-09-16 09:16:46 -0400

    distribute candies problem solved

---
+++ src/main/java/DistributeCandies.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class DistributeCandies {
+
+    public static int candy(int[] ratings) {
+        int n = ratings.length;
+
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1);
+
+        //left pass
+        for (int i=1; i< ratings.length;i++){
+            if(ratings[i]> ratings[i-1]) {
+                candies[i] = candies[i-1] + 1;
+            }
+        }
+
+        //right pass
+        for (int i= n-2 ; i>=0 ;i--){
+            if(ratings[i]> ratings[i+1]) {
+                candies[i] = Math.max(candies[i], candies[i+1] + 1);
+            }
+        }
+        int totalCandies = 0;
+        for (int candy: candies){
+            totalCandies+=candy;
+        }
+        return totalCandies;
+    }
+
+    public static void main(String[] args) {
+        int[] ratings1 = {1, 0, 2};
+        System.out.println(candy(ratings1));  // Output: 5
+
+        int[] ratings2 = {1, 2, 2};
+        System.out.println(candy(ratings2));  // Output: 4
+    }
+}
