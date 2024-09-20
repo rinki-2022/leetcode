ee99a9b2a7c3b71a6fd34a8f7f921fd33f696280
rinki-2022 <rinkibansal2022@gmail.com>
2024-09-19 22:21:11 -0400

    solved Minimum sorted array problem

---
+++ src/main/java/FindMinimumSortedArray.java
@@ -0,0 +1,29 @@
+public class FindMinimumSortedArray {
+        public int findMin(int[] nums) {
+            int left = 0;
+            int right = nums.length - 1;
+
+            // this condition tells array is sorted and not rotated
+            if (nums[left] < nums[right]) {
+                return nums[left];
+            }
+
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+
+                if (nums[mid] > nums[right]) { //ele has to be in right half
+                        left = mid +1;
+                } else {
+                        right = mid;//ele has to be in left half
+                }
+            }
+            return nums[left];
+        }
+
+        public static void main(String[] args) {
+            FindMinimumSortedArray solution = new FindMinimumSortedArray();
+
+            int[] nums = {3, 4, 5, 1, 2};
+            System.out.println("Minimum: " + solution.findMin(nums)); // Output: 1
+        }
+    }
