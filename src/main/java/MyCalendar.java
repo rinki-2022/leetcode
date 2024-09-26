2c8791156f3d4138691e6a5dc3a849f63105145a
rinki-2022 <rinkibansal2022@gmail.com>
2024-09-26 16:27:35 -0400

    solve mycalendar problem

---
+++ src/main/java/MyCalendar.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MyCalendar {
+
+    private List<int[]> bookings;
+
+    public MyCalendar() {
+        bookings = new ArrayList<>();
+    }
+
+    public boolean book(int start, int end) {
+
+        if (bookings.size() == 0){
+            bookings.add(new int[]{start, end});
+            return true;
+        } else {
+            for (int [] singleBooking : bookings){
+                if (start < singleBooking[1] && end > singleBooking[0]){
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+    public static void main(String[] args) {
+        MyCalendar myCalendar = new MyCalendar();
+        System.out.println(myCalendar.book(10, 20)); // true
+        System.out.println(myCalendar.book(15, 25)); // false
+        System.out.println(myCalendar.book(20, 30)); // true
+    }
+
+}
