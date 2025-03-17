dda18fdb90866a049d439a0635c8fc3f4e72ae63
rinki-2022 <rinkibansal2022@gmail.com>
2025-03-17 16:08:11 -0400

    problem 2 done - blind 75

--- src/main/java/Ques2BestTimeToBuyAndSellStock.java
+++ src/main/java/Ques2BestTimeToBuyAndSellStock.java
@@ -1,27 +1,20 @@
 class Ques2BestTimeToBuyAndSellStock {
     public static int maxProfit(int[] prices) {
-        // Solution to be implemented by Avyaan
-        return 0;
+        int maxProfit = Integer.MIN_VALUE;
+        int minValue = Integer.MAX_VALUE;
+        for (int i=0; i< prices.length; i++){
+            minValue = Math.min(prices[i] , minValue);
+            maxProfit = Math.max(prices[i] - minValue, maxProfit);
+        }
+        return maxProfit;
     }
 
     public static void main(String[] args) {
-        int[][] testCases = {
-                {7, 1, 5, 3, 6, 4},
-                {7, 6, 4, 3, 1},
-                {1, 2, 3, 4, 5},
-                {3, 2, 6, 5, 0, 3},
-                {2, 4, 1},
-                {2, 1, 2, 1, 2},
-                {1, 2, 10, 1, 10},
-                {1, 6, 7, 2, 3},
-                {1, 9, 3, 10, 4},
-                {2, 5, 1, 8, 6}
-        };
-        int[] expectedResults = {5, 0, 4, 4, 2, 1, 9, 6, 9, 7};
+        int[] testCase = {7, 1, 5, 3, 6, 4};
+        int expectedResult = 5;
 
-        for (int i = 0; i < testCases.length; i++) {
-            int result = maxProfit(testCases[i]);
-            System.out.println("Test " + (i + 1) + ": " + (result == expectedResults[i] ? "Passed" : "Failed"));
-        }
+        int result = maxProfit(testCase);
+        System.out.println("Test 1: " + (result == expectedResult ? "Passed" : "Failed"));
     }
+
 }
