cda64d5789420737aa883177219d529f6bd0f886
rinki-2022 <rinkibansal2022@gmail.com>
2025-03-17 14:48:44 -0400

    problem 3 done - blind 75

--- src/main/java/Ques2BestTimeToBuyAndSellStock.java
+++ src/main/java/Ques2BestTimeToBuyAndSellStock.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+class Ques2BestTimeToBuyAndSellStock {
+    public static int maxProfit(int[] prices) {
+        // Solution to be implemented by Avyaan
+        return 0;
+    }
 
-public class Ques2BestTimeToBuyAndSellStock {
+    public static void main(String[] args) {
+        int[][] testCases = {
+                {7, 1, 5, 3, 6, 4},
+                {7, 6, 4, 3, 1},
+                {1, 2, 3, 4, 5},
+                {3, 2, 6, 5, 0, 3},
+                {2, 4, 1},
+                {2, 1, 2, 1, 2},
+                {1, 2, 10, 1, 10},
+                {1, 6, 7, 2, 3},
+                {1, 9, 3, 10, 4},
+                {2, 5, 1, 8, 6}
+        };
+        int[] expectedResults = {5, 0, 4, 4, 2, 1, 9, 6, 9, 7};
+
+        for (int i = 0; i < testCases.length; i++) {
+            int result = maxProfit(testCases[i]);
+            System.out.println("Test " + (i + 1) + ": " + (result == expectedResults[i] ? "Passed" : "Failed"));
+        }
+    }
 }
--- src/main/java/Ques3ContainsDuplicate.java
+++ src/main/java/Ques3ContainsDuplicate.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
-public class Ques3ContainsDuplicate {
+class Ques3ContainsDuplicate {
+    public static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+
+        for (int i=1; i< nums.length;i++){
+            if (nums[i] == nums[i-1]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] testCase1 = {1, 2, 3, 1};
+        boolean expectedResult1 = true;
+
+        int[] testCase2 = {1, 2, 3, 4};
+        boolean expectedResult2 = false;
+
+        System.out.println("Test 1: " + (containsDuplicate(testCase1) == expectedResult1 ? "Passed" : "Failed"));
+        System.out.println("Test 2: " + (containsDuplicate(testCase2) == expectedResult2 ? "Passed" : "Failed"));
+    }
 }
